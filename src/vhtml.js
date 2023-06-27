1ed1bd80141568515c10df4103f62b620777252e
Odin H B <odinhb@protonmail.com>
2023-06-27 16:03:47 +0200

    Rework into String-class based solution.
    This was suggested by @remziatay on github.
    
    I basically rewrote the code based on the previous version.

--- src/vhtml.js
+++ src/vhtml.js
@@ -1,60 +1,116 @@
-import emptyTags from './empty-tags';
+const HTML_ESCAPE_MAP = {'&':'amp','<':'lt','>':'gt','"':'quot',"'":'apos'};
+// sanitize/escape an html string
+function esc(str){
+  return String(str).replace(/[&<>"']/g, s=>`&${HTML_ESCAPE_MAP[s]};`);
+}
 
-// escape an attribute
-let esc = str => String(str).replace(/[&<>"']/g, s=>`&${map[s]};`);
-let map = {'&':'amp','<':'lt','>':'gt','"':'quot',"'":'apos'};
 let setInnerHTMLAttr = 'dangerouslySetInnerHTML';
 let DOMAttributeNames = {
-	className: 'class',
-	htmlFor: 'for'
+  className: 'class',
+  htmlFor: 'for'
 };
 
-let sanitized = {};
+// Void tags or self closing tags don't require having content.
+// They are often written as <tag />.
+// https://developer.mozilla.org/en-US/docs/Glossary/Void_element
+const VOID_TAGS = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+];
 
-/** Hyperscript reviver that constructs a sanitized HTML string. */
-export default function h(name, attrs) {
-	let stack=[], s = '';
-	attrs = attrs || {};
-	for (let i=arguments.length; i-- > 2; ) {
-		stack.push(arguments[i]);
-	}
-
-	// Sortof component support!
-	if (typeof name==='function') {
-		attrs.children = stack.reverse();
-		return name(attrs);
-		// return name(attrs, stack.reverse());
-	}
-
-	if (name) {
-		s += '<' + name;
-		if (attrs) for (let i in attrs) {
-			if (attrs[i]!==false && attrs[i]!=null && i !== setInnerHTMLAttr) {
-				s += ` ${DOMAttributeNames[i] ? DOMAttributeNames[i] : esc(i)}="${esc(attrs[i])}"`;
-			}
-		}
-		s += '>';
-	}
-
-	if (emptyTags.indexOf(name) === -1) {
-		if (attrs[setInnerHTMLAttr]) {
-			s += attrs[setInnerHTMLAttr].__html;
-		}
-		else while (stack.length) {
-			let child = stack.pop();
-			if (child) {
-				if (child.pop) {
-					for (let i=child.length; i--; ) stack.push(child[i]);
-				}
-				else {
-					s += sanitized[child]===true ? child : esc(child);
-				}
-			}
-		}
-
-		s += name ? `</${name}>` : '';
-	}
-
-	sanitized[s] = true;
-	return s;
+function extractChildren(args) {
+  return Array.from(args)
+    .slice(2)
+    .reverse()
+  ;
 }
+
+function renderAttributes(attributes) {
+  let result = '';
+
+  for(const property in attributes) {
+    const value = attributes[property];
+
+    if (property === setInnerHTMLAttr) continue;
+    if (value === false) continue;
+    if (value == null) continue;
+
+    result += ' ';
+    if (DOMAttributeNames[property]) {
+      result += DOMAttributeNames[property];
+    } else {
+      result += `${esc(property)}`;
+    }
+    result += `="${esc(value)}"`;
+  }
+
+  return result;
+}
+
+function render(tagName, attributes) {
+  attributes = attributes || {};
+  const children = extractChildren(arguments);
+
+  if (typeof tagName === 'function') {
+    const pseudoComponent = tagName;
+    attributes.children = children.reverse();
+    return pseudoComponent(attributes);
+  }
+
+  let result = '';
+
+  if (tagName) { // null is passed when rendering a fragment
+    result += '<';
+
+    result += tagName;
+
+    result += renderAttributes(attributes);
+
+    result += '>';
+  }
+
+  if (!VOID_TAGS.includes(tagName)) {
+    if (attributes[setInnerHTMLAttr]) {
+      result += attributes[setInnerHTMLAttr].__html;
+    } else while(children.length) {
+      const child = children.pop();
+      if (child) {
+        if (child.pop) {
+          for (let i = child.length - 1; i >= 0; i--) {
+            children.push(child[i]);
+          }
+        } else {
+          if (child._vvhtml_safe) {
+            result += child;
+          } else {
+            result += esc(child);
+          }
+        }
+      }
+    }
+
+    if (tagName) result += `</${tagName}>`;
+  }
+
+  // Read about the fun world of javascript strings
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#string_primitives_and_string_objects
+  const s = new String(result);
+  s._vvhtml_safe = true;
+  return s;
+}
+
+export default render;
--- test/vhtml.js
+++ test/vhtml.js
@@ -4,190 +4,246 @@ import { expect } from 'chai';
 /*global describe,it*/
 
 describe('vhtml', () => {
-	it('should stringify html', () => {
-		let items = ['one', 'two', 'three'];
-		expect(
-			<div class="foo">
-				<h1>Hi!</h1>
-				<p>Here is a list of {items.length} items:</p>
-				<ul>
-					{ items.map( item => (
-						<li>{ item }</li>
-					)) }
-				</ul>
-			</div>
-		).to.equal(
-			`<div class="foo"><h1>Hi!</h1><p>Here is a list of 3 items:</p><ul><li>one</li><li>two</li><li>three</li></ul></div>`
-		);
-	});
+  it('produces a string', () => {
+    expect(
+      <div class="foo"></div>.toString()
+    ).to.equal('<div class="foo"></div>');
+  });
 
-	it('should sanitize children', () => {
-		expect(
-			<div>
-				{ `<strong>blocked</strong>` }
-				<em>allowed</em>
-			</div>
-		).to.equal(
-			`<div>&lt;strong&gt;blocked&lt;/strong&gt;<em>allowed</em></div>`
-		);
-	});
+  it('handles child elements', () => {
+    expect(
+      <div>
+        <div>
+          <div><p></p></div>
+        </div>
+      </div>.toString()
+    ).to.equal('<div><div><div><p></p></div></div></div>');
+  });
 
-	it('should sanitize attributes', () => {
-		expect(
-			<div onclick={`&<>"'`} />
-		).to.equal(
-			`<div onclick="&amp;&lt;&gt;&quot;&apos;"></div>`
-		);
-	});
+  it('supports attributes', () => {
+    expect(
+      <div data-attr="yes">
+        <p good="true"></p>
+        <div>
+          <p bad="yes"></p>
+        </div>
+      </div>.toString()
+    ).to.equal(
+      '<div data-attr="yes"><p good="true"></p><div><p bad="yes"></p></div></div>',
+    );
+  });
 
-	it('should not sanitize the "dangerouslySetInnerHTML" attribute, and directly set its `__html` property as innerHTML', () => {
-		expect(
-			<div dangerouslySetInnerHTML={{ __html: "<span>Injected HTML</span>" }} />
-		).to.equal(
-			`<div><span>Injected HTML</span></div>`
-		);
-	});
+  it('sanitizes hardcoded attributes', () => {
+    expect(
+      <div lol="&<>'"></div>.toString()
+    ).to.equal('<div lol="&amp;&lt;&gt;&apos;"></div>');
+  });
 
-	it('should flatten children', () => {
-		expect(
-			<div>
-				{[['a','b']]}
-				<c>d</c>
-				{['e',['f'],[['g']]]}
-			</div>
-		).to.equal(
-			`<div>ab<c>d</c>efg</div>`
-		);
-	});
+  it('sanitizes dynamic attributes', () => {
+    expect(
+      <div onclick={`&<>"'`}></div>.toString()
+    ).to.equal(
+      `<div onclick="&amp;&lt;&gt;&quot;&apos;"></div>`
+    );
+  });
 
-	it('should support sortof components', () => {
-		let items = ['one', 'two'];
+  it('sanitizes dynamic children', () => {
+    expect(
+      <div>
+        { `<strong>blocked</strong>` }
+      </div>.toString()
+    ).to.equal(
+      `<div>&lt;strong&gt;blocked&lt;/strong&gt;</div>`
+    );
+  });
 
-		const Item = ({ item, index, children }) => (
-			<li id={index}>
-				<h4>{item}</h4>
-				{children}
-			</li>
-		);
+  it('should not sanitize the "dangerouslySetInnerHTML" attribute, and directly set its `__html` property as innerHTML', () => {
+    expect(
+      <div dangerouslySetInnerHTML={{ __html: "<span>Injected HTML</span>" }} />.toString()
+    ).to.equal(
+      `<div><span>Injected HTML</span></div>`
+    );
+  });
 
-		expect(
-			<div class="foo">
-				<h1>Hi!</h1>
-				<ul>
-					{ items.map( (item, index) => (
-						<Item {...{ item, index }}>
-							This is item {item}!
-						</Item>
-					)) }
-				</ul>
-			</div>
-		).to.equal(
-			`<div class="foo"><h1>Hi!</h1><ul><li id="0"><h4>one</h4>This is item one!</li><li id="1"><h4>two</h4>This is item two!</li></ul></div>`
-		);
-	});
+  it('renders a list of items', () => {
+    let items = ['one', 'two', 'three'];
+    expect(
+      <div class="foo">
+        <h1>Hi!</h1>
+        <p>Here is a list of {items.length} items:</p>
+        <ul>
+          { items.map( item => (
+            <li>{ item }</li>
+          )) }
+        </ul>
+      </div>.toString()
+    ).to.equal(
+      `<div class="foo"><h1>Hi!</h1><p>Here is a list of 3 items:</p><ul><li>one</li><li>two</li><li>three</li></ul></div>`,
+    );
+  });
 
-	it('should support sortof components without args', () => {
-	  let items = ['one', 'two'];
+  it('should flatten children', () => {
+    expect(
+      <div>
+        {[['a','b']]}
+        <c>d</c>
+        {['e',['f'],[['g']]]}
+      </div>.toString()
+    ).to.equal(
+      `<div>ab<c>d</c>efg</div>`
+    );
+  });
 
-	  const Item = () => (
-	    <li>
-	      <h4></h4>
-	    </li>
-	  );
+  it('should support sortof components', () => {
+    let items = ['one', 'two'];
 
-	  expect(
-	    <div class="foo">
-	      <h1>Hi!</h1>
-	      <ul>
-	        { items.map( (item, index) => (
-	          <Item>
-	            This is item {item}!
-	          </Item>
-	        )) }
-	      </ul>
-	    </div>
-	  ).to.equal(
-	    `<div class="foo"><h1>Hi!</h1><ul><li><h4></h4></li><li><h4></h4></li></ul></div>`
-	  );
-	});
+    const Item = ({ item, index, children }) => (
+      <li id={index}>
+        <h4>{item}</h4>
+        {children}
+      </li>
+    );
 
-	it('should support sortof components without args but with children', () => {
-	  let items = ['one', 'two'];
+    expect(
+      <div class="foo">
+        <h1>Hi!</h1>
+        <ul>
+          { items.map( (item, index) => (
+            <Item {...{ item, index }}>
+              This is item {item}!
+            </Item>
+          )) }
+        </ul>
+      </div>.toString()
+    ).to.equal(
+      `<div class="foo"><h1>Hi!</h1><ul><li id="0"><h4>one</h4>This is item one!</li><li id="1"><h4>two</h4>This is item two!</li></ul></div>`
+    );
+  });
 
-	  const Item = ({ children }) => (
-	    <li>
-	      <h4></h4>
-	      {children}
-	    </li>
-	  );
+  it('should support sortof components without args', () => {
+    let items = ['one', 'two'];
 
-	  expect(
-	    <div class="foo">
-	      <h1>Hi!</h1>
-	      <ul>
-	        { items.map( (item, index) => (
-	          <Item>
-	            This is item {item}!
-	          </Item>
-	        )) }
-	      </ul>
-	    </div>
-	  ).to.equal(
-	    `<div class="foo"><h1>Hi!</h1><ul><li><h4></h4>This is item one!</li><li><h4></h4>This is item two!</li></ul></div>`
-	  );
-	});
+    const Item = () => (
+      <li>
+        <h4></h4>
+      </li>
+    );
 
-	it('should support empty (void) tags', () => {
-		expect(
-			<div>
-				<area />
-				<base />
-				<br />
-				<col />
-				<command />
-				<embed />
-				<hr />
-				<img />
-				<input />
-				<keygen />
-				<link />
-				<meta />
-				<param />
-				<source />
-				<track />
-				<wbr />
-				{/* Not void elements */}
-				<div />
-				<span />
-				<p />
-			</div>
-		).to.equal(
-			`<div><area><base><br><col><command><embed><hr><img><input><keygen><link><meta><param><source><track><wbr><div></div><span></span><p></p></div>`
-		);
-	});
+    expect(
+      <div class="foo">
+        <h1>Hi!</h1>
+        <ul>
+          { items.map( (item, index) => (
+            <Item>
+              This is item {item}!
+            </Item>
+          )) }
+        </ul>
+      </div>.toString()
+    ).to.equal(
+      `<div class="foo"><h1>Hi!</h1><ul><li><h4></h4></li><li><h4></h4></li></ul></div>`
+    );
+  });
 
-	it('should handle special prop names', () => {
-		expect(
-			<div className="my-class" htmlFor="id" />
-		).to.equal(
-			'<div class="my-class" for="id"></div>'
-		);
-	});
+  it('should support sortof components without args but with children', () => {
+    let items = ['one', 'two'];
 
-	it('should support string fragments', () => {
-		expect(
-			h(null, null, "foo", "bar", "baz")
-		).to.equal(
-			'foobarbaz'
-		);
-	});
+    const Item = ({ children }) => (
+      <li>
+        <h4></h4>
+        {children}
+      </li>
+    );
 
-	it('should support element fragments', () => {
-		expect(
-			h(null, null, <p>foo</p>, <em>bar</em>, <div class="qqqqqq">baz</div>)
-		).to.equal(
-			'<p>foo</p><em>bar</em><div class="qqqqqq">baz</div>'
-		);
-	});
+    expect(
+      <div class="foo">
+        <h1>Hi!</h1>
+        <ul>
+          { items.map( (item, index) => (
+            <Item>
+              This is item {item}!
+            </Item>
+          )) }
+        </ul>
+      </div>.toString()
+    ).to.equal(
+      `<div class="foo"><h1>Hi!</h1><ul><li><h4></h4>This is item one!</li>` +
+      `<li><h4></h4>This is item two!</li></ul></div>`
+    );
+  });
 
+  it('supports void tags', () => {
+    expect(
+      <div>
+        <area />
+        <base />
+        <br />
+        <col />
+        <command />
+        <embed />
+        <hr />
+        <img />
+        <input />
+        <keygen />
+        <link />
+        <meta />
+        <param />
+        <source />
+        <track />
+        <wbr />
+      </div>.toString()
+    ).to.equal(
+      `<div><area><base><br><col><command><embed><hr><img><input><keygen>` +
+      `<link><meta><param><source><track><wbr></div>`
+    );
+  });
+
+  it('expands self closing tags', () => {
+    expect(
+      <div>
+        <div />
+        <span />
+        <p />
+      </div>.toString()
+    ).to.equal(
+      `<div><div></div><span></span><p></p></div>`
+    )
+  });
+
+  it('should handle special prop names', () => {
+    expect(
+      <div className="my-class" htmlFor="id" />.toString()
+    ).to.equal(
+      '<div class="my-class" for="id"></div>'
+    );
+  });
+
+  it('should support string fragments', () => {
+    expect(
+      h(null, null, "foo", "bar", "baz").toString()
+    ).to.equal(
+      'foobarbaz'
+    );
+  });
+
+  it('should support element fragments', () => {
+    expect(
+      h(null, null, <p>foo</p>, <em>bar</em>, <div class="qqqqqq">baz</div>).toString()
+    ).to.equal(
+      '<p>foo</p><em>bar</em><div class="qqqqqq">baz</div>'
+    );
+  });
+
+  // regression test for https://github.com/developit/vhtml/issues/34
+  it('does not allow cache-based html injection anymore', () => {
+    const injectable = '<h1>test</h1>';
+
+    const a = <div>{injectable}</div>;
+    const b = <div><h1>test</h1></div>;
+    const c = <div>{injectable}</div>;
+
+    expect(a.toString()).to.equal('<div>&lt;h1&gt;test&lt;/h1&gt;</div>');
+    expect(b.toString()).to.equal('<div><h1>test</h1></div>');
+    expect(c.toString()).to.equal('<div>&lt;h1&gt;test&lt;/h1&gt;</div>');
+  });
 });
